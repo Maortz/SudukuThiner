fbc7e78147e2af768afd2e6910a4339ac991a59d
Maortz <maortz@gmail.com>
2016-04-13 19:53:30 +0300

    Start

---
+++ Thiner/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thiner
+{
+    class Cell
+    {
+        public int Num { get; set; }
+        public bool IsReadonly { get; set; }
+        public readonly Position Pos;
+
+        public Cell(int x, int y)
+        {
+            Num = 0;
+            IsReadonly = false;
+            Pos = new Position { X = x, Y = y };
+        }
+
+        public bool Advance()
+        {
+            if (IsReadonly)
+                return false;
+            Num++;
+            if(Num > 9)
+            {
+                Num = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Thiner/Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thiner
+{
+    class Table
+    {
+        Cell[,] table = new Cell[9,9];
+        //
+
+        public Table(Cell[] cell_array)
+        {
+            int count = 0;
+            int row = 0, col = 0;
+            while (count < cell_array.Length)
+            {
+                table[row, col] = cell_array[count];
+                count++;
+                row++;
+                col++;
+                if (col == 9)
+                {
+                    col = 0;
+                    row++;
+                    if (row == 9)
+                        break;
+                }
+            }
+        }
+
+        public Cell GetPrev(Position pos)
+        {
+            throw new Exception("Unimpl");
+        }
+
+        public Cell GetNext(Position pos)
+        {
+            throw new Exception("Unimpl");
+        }
+
+        public bool IsValid(Cell cell)
+        {
+            throw new Exception("Unimpl");
+        }
+    }
+}
